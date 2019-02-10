a95b8de66ed84b3c37ee9cf0b48567bcbabeddf8
IhateProprietary <death2511@hotmail.com>
2019-02-10 18:05:58 +0100

    [Warning]!!!! Project was not intended for human to read Collen done Grace done Sully done

--- C/Grace.c
+++ C/Grace.c
@@ -1,3 +1,6 @@
 #include <stdio.h>
-#define val "#include <stdio.h>%3$c#define val %2$c%1$s%2$c%3$c#define MAIN(x) int main%3$c"
-#define MAIN(x) int main(){}
+#include <unistd.h>
+#include <fcntl.h>
+#define val "#include <stdio.h>%3$c#include <unistd.h>%3$c#include <fcntl.h>%3$c#define val %1$c%2$s%1$c%3$c#define MAIN(x) int main(){int fd=open(%1$cGrace_kid.c%1$c,O_WRONLY|O_TRUNC|O_CREAT,0644);if(fd>2){dprintf(fd,x,34,x,10);}close(fd);}%3$cMAIN(val);%3$c"
+#define MAIN(x) int main(){int fd=open("Grace_kid.c",O_WRONLY|O_TRUNC|O_CREAT,0644);if(fd>2){dprintf(fd,x,34,x,10);}close(fd);}
+MAIN(val)
